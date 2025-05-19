8b56df4a667be65b2107ca86d32107ed1624ecd2
Henryck <guaramatohenryck@gmail.com>
2025-05-18 20:36:20 -0400

    feat: integrate Clerk and Node adapter into Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import node from "@astrojs/node";
+import clerk from "@clerk/astro";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  integrations: [clerk()],
+  adapter: node({ mode: "standalone" }),
+  output: "server",
 });
