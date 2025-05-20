0e496379c6cc041f81abccc011dbc2ba966eb5c3
Henryck <guaramatohenryck@gmail.com>
2025-05-19 23:09:43 -0400

    feat: integrate Clerk authentication with localized themes and create middleware for protected routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,20 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import node from "@astrojs/node";
 import clerk from "@clerk/astro";
+import { dark } from '@clerk/themes'
+import { esMX } from '@clerk/localizations';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-  integrations: [clerk()],
+  integrations: [clerk({
+    localization: esMX,
+    appearance: {
+      baseTheme: dark,
+    }
+  })],
   adapter: node({ mode: "standalone" }),
-  output: "server",
+  output: "server"
 });
